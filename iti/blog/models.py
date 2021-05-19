e9d89abf7c529cc664fb572f9b7beb7492d45d4e
Mahmoud Hassan <hsn.mhmod.96@gmail.com>
2021-05-19 21:27:13 +0200

    add categories

--- iti/blog/models.py
+++ iti/blog/models.py
@@ -23,13 +23,9 @@ class Post(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     content = models.TextField()
     author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
-    
-    # category = models.ForeignKey(Category, on_delete=models.CASCADE)
-
-
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, default=1)
     updated_on = models.DateTimeField(auto_now= True)
     created_on = models.DateTimeField(auto_now_add=True)
-
     status = models.IntegerField(choices=STATUS, default=0)
 
     class Meta:
